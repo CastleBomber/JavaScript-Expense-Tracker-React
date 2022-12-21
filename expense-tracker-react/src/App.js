8e99f830a7c0b93b21befcfaf03fc8ecc11f3a47
cBombs <richie.ucsb@gmail.com>
2022-12-20 20:44:48 -0800

    hooks

---
+++ expense-tracker-react/src/App.js
@@ -0,0 +1,27 @@
+/**
+ * Author: CastleBomber
+ * Date: December 14th, 2022
+ *
+ * Tip: cmd+D will change opening and closing tag simultaneously
+ */
+import "./App.css";
+import React from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
